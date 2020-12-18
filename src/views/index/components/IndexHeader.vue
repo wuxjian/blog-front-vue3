54cf240084ee07f5580521676ca37eda15f109bb
wuxjian <wu_xjian@163.com>
2020-12-18 14:47:47 +0800

    '完善首页'

--- src/views/index/components/IndexHeader.vue
+++ src/views/index/components/IndexHeader.vue
@@ -1,43 +1,67 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-white shadow">
-    <div class="container-fluid">
+    <div class="container">
       <a class="navbar-brand" href="#">
         <img src="@/assets/logo.png" alt="" height="34">
-        <span class="slogan">何以解忧，惟有暴富！</span>
+        <span class="slogan text-primary">何以解忧，惟有暴富！</span>
       </a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
               aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">首页</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">工具</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">管理</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">关于</a>
+      <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+        <ul class="navbar-nav mb-2 mb-lg-0">
+          <li v-for="(item, index) in navItemList" :key="index" class="nav-item px-1" @click="handleItemClick(item)">
+            <a :class="currentKey === item.key ? 'active' : ''" class="nav-link" aria-current="page" href="#">
+              {{item.name}}
+            </a>
           </li>
         </ul>
-        <form class="d-flex">
-          <input class="form-control me-2 search-input" type="search" placeholder="" aria-label="Search">
-          <button class="btn btn-outline-primary search-button" type="submit">搜索</button>
-        </form>
       </div>
     </div>
   </nav>
 </template>
 
 <script lang="ts">
-import {defineComponent} from 'vue';
+import {defineComponent, ref} from 'vue';
+interface NavItem {
+  key: string;
+  name: string;
+}
+const navItemList: Array<NavItem> = [
+  {
+    key: 'home',
+    name: '首页',
+  },
+  {
+    key: 'tool',
+    name: '工具',
+  },
+  {
+    key: 'manage',
+    name: '管理',
+  },
+  {
+    key: 'about',
+    name: '关于',
+  },
+]
 
 export default defineComponent({
   name: 'IndexHeader',
+  setup() {
+
+    const currentKey = ref('home')
+    const handleItemClick = (item: NavItem) => {
+      currentKey.value = item.key
+    }
+
+    return {
+      currentKey,
+      navItemList,
+      handleItemClick
+    }
+  }
 });
 </script>
 
@@ -50,7 +74,6 @@ export default defineComponent({
 .navbar-brand .slogan {
   margin-left: 10px;
   font-size: 12px;
-  color: $main-color;
 }
 .search-button {
   width: 80px;
